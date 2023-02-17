f28a7c82c3ce305960d9a43ce7b5f1b3d60d2fd4
alemarg99 <125296985+alemarg99@users.noreply.github.com>
2023-02-17 09:31:34 +0100

    Add files via upload

---
+++ second.java
@@ -0,0 +1,7 @@
+public class second {
+    public static void main (String[] args) {
+        System.out.println("Ciao");
+        System.out.println("ale");
+        System.out.println("marggg");
+}
+}
