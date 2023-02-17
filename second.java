2c0648a1d6d6892abd6d9517294473a31123b126
alemarg99 <125296985+alemarg99@users.noreply.github.com>
2023-02-17 09:51:38 +0100

    Update second.java

--- second.java
+++ second.java
@@ -1,7 +1,7 @@
 public class second {
     public static void main (String[] args) {
         System.out.println("Ciao");
-        System.out.println("ale");
+        System.out.println("ale"); //commento
         System.out.println("marggg");
 }
 }
